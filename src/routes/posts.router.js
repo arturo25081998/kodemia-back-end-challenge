36f8b8d1a3c4e25245466aab9dda159796c74189
Arturo Juarez <ajrobles98@gmail.com>
2024-08-17 14:27:58 -0600

    feat : Creating routes to the project

---
+++ src/routes/posts.router.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const postUseCases = require("../usecases/posts.usecases");
+const createError = require("http-errors");
+const router = express.Router();
+const auth = require("../middlewares/auth");
+
+router.delete("/:id", auth, async (request, response) => {
+  try {
+    const id = request.params.id;
+    const post = await postUseCases.getById(id);
+    if (post.user != request.user.id) {
+      throw createError(400, "Only the creator of this post can deleted");
+    }
+    const postDeleted = await postUseCases.deleteById(id);
+    response.json({
+      success: true,
+      message: "Post deleted by id",
+      data: { post: postDeleted.id },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.patch("/:id", auth, async (request, response) => {
+  try {
+    const id = request.params.id;
+    const postData = request.body;
+    const postUpdated = await postUseCases.updateById(id, postData);
+    response.json({
+      success: true,
+      message: "Post updated",
+      data: { post: postUpdated },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.post("/", auth, async (request, response) => {
+  try {
+    const postData = request.body;
+    postData.user = request.user.id;
+    const newPost = await postUseCases.create(postData);
+    response.json({
+      success: true,
+      message: "Post created",
+      data: { post: newPost },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.get("/", auth, async (request, response) => {
+  try {
+    const posts = await postUseCases.getAll();
+    response.json({
+      success: true,
+      message: "All post",
+      data: { posts },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.get("/:search", auth, async (request, response) => {
+  try {
+    const search = request.params.search;
+    const posts = await postUseCases.getAll(search);
+    response.json({
+      success: true,
+      message: "All post",
+      data: { posts },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -20,14 +20,17 @@ router.get("/", async (request, response) => {
   }
 });
 
-router.post("/", async (request, response) => {
+router.get("/:id", async (request, response) => {
   try {
-    const userData = request.body;
-    const newUser = await userUseCases.create(userData);
+    const id = request.params.id;
+    const user = await userUseCases.getById(id);
+    if (!user) {
+      throw createError(404, "User not found");
+    }
     response.json({
       success: true,
-      message: "User Created",
-      data: { user: newUser },
+      message: "User by id",
+      data: { user },
     });
   } catch (error) {
     response.status(error.status || 500);
@@ -37,3 +40,41 @@ router.post("/", async (request, response) => {
     });
   }
 });
+
+router.post("/", async (request, response) => {
+  try {
+    const userData = request.body;
+    const newUser = await userUseCases.create(userData);
+    response.json({
+      success: true,
+      message: "User Created",
+      data: { user: newUser.id },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.post("/login", async (request, response) => {
+  try {
+    const userData = request.body;
+    const token = await userUseCases.login(userData);
+    response.json({
+      success: true,
+      message: "Koder logged",
+      data: { token },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
