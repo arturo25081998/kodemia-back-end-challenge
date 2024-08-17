3ddd3f458cf1392606bd2b44ec5597dae5bcdb67
Arturo Juarez <ajrobles98@gmail.com>
2024-08-17 17:32:44 -0600

    feat : Adding vercel access

--- src/lib/server.js
+++ src/lib/server.js
@@ -6,7 +6,11 @@ const usersRoutes = require("../routes/users.router");
 const postRoutes = require("../routes/posts.router");
 const authRoutes = require("../routes/auth.router");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://desafio-maquetado-three.vercel.app/",
+  })
+);
 app.use(helmet());
 app.use(express.json());
 
