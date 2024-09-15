9b9406e67b8f51e58654612d9105e95605f83df0
Arturo Juarez <ajrobles98@gmail.com>
2024-09-15 08:56:03 -0600

    feat : adding id user to login response

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const userUseCases = require("../usecases/users.usecases");
 const createError = require("http-errors");
+const jwt = require("../lib/jwt");
 const router = express.Router();
 
 router.post("/login", async (request, response) => {
   try {
     const userData = request.body;
     const token = await userUseCases.login(userData);
+    const decode = jwt.verify(token);
+    const id = decode.id;
     response.json({
       success: true,
       message: "User logged",
-      data: { token },
+      data: { token, id },
     });
   } catch (error) {
     response.status(error.status || 500);
